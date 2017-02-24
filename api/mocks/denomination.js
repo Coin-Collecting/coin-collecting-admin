2054c3a0dfafc39b90ad12cc1cb8ec67b8677626
thewickedwebdev <syoung@paintnite.com>
2017-02-24 01:36:52 -0500

    updated

--- api/mocks/denomination.js
+++ api/mocks/denomination.js
@@ -1,32 +1,32 @@
 export default {
   "1": {
     "id": "1",
-    "type": "HALF_CENTS",
+    "type": "HALF_CENT",
     "value": 0.005
   },
   "2": {
     "id": "2",
-    "type": "LARGE_CENTS",
+    "type": "LARGE_CENT",
     "value": 0.01
   },
   "3": {
     "id": "3",
-    "type": "SMALL_CENTS",
+    "type": "SMALL_CENT",
     "value": 0.01
   },
   "4": {
     "id": "4",
-    "type": "TWO_CENT_PIECES",
+    "type": "TWO_CENT_PIECE",
     "value": 0.02
   },
   "5": {
     "id": "5",
-    "type": "NICKEL_THREE_CENT_PIECES",
+    "type": "NICKEL_THREE_CENT_PIECE",
     "value": 0.03
   },
   "6": {
     "id": "6",
-    "type": "SILVER_THREE_CENT_PIECES",
+    "type": "SILVER_THREE_CENT_PIECE",
     "value": 0.03
   },
   "7": {
@@ -36,67 +36,67 @@ export default {
   },
   "8": {
     "id": "8",
-    "type": "HALF_DIMES",
+    "type": "HALF_DIME",
     "value": 0.05
   },
   "9": {
     "id": "9",
-    "type": "DIMES",
+    "type": "DIME",
     "value": 0.1
   },
   "10": {
     "id": "10",
-    "type": "TWENTY_CENT_PIECES",
+    "type": "TWENTY_CENT_PIECE",
     "value": 0.2
   },
   "11": {
     "id": "11",
-    "type": "QUARTER_DOLLARS",
+    "type": "QUARTER_DOLLAR",
     "value": 0.25
   },
   "12": {
     "id": "12",
-    "type": "HALF_DOLLARS",
+    "type": "HALF_DOLLAR",
     "value": 0.5
   },
   "13": {
     "id": "13",
-    "type": "DOLLARS",
+    "type": "DOLLAR",
     "value": 1
   },
   "14": {
     "id": "14",
-    "type": "GOLD_DOLLARS",
+    "type": "GOLD_DOLLAR",
     "value": 1
   },
   "15": {
     "id": "15",
-    "type": "QUARTER_EAGLES",
+    "type": "QUARTER_EAGLE",
     "value": 2.5
   },
   "16": {
     "id": "16",
-    "type": "THREE_DOLLAR_GOLD_PIECES",
+    "type": "THREE_DOLLAR_GOLD_PIECE",
     "value": 3
   },
   "17": {
     "id": "17",
-    "type": "FOUR_DOLLAR_GOLD_PIECES",
+    "type": "FOUR_DOLLAR_GOLD_PIECE",
     "value": 4
   },
   "18": {
     "id": "18",
-    "type": "HALF_EAGLES",
+    "type": "HALF_EAGLE",
     "value": 5
   },
   "19": {
     "id": "19",
-    "type": "EAGLES",
+    "type": "EAGLE",
     "value": 10
   },
   "20": {
     "id": "20",
-    "type": "DOUBLE_EAGLES",
+    "type": "DOUBLE_EAGLE",
     "value": 20
   }
 }
