57ab6b7c3e8d5e75c232b341eee432b161731623
thewickedwebdev <syoung@paintnite.com>
2017-02-24 15:10:57 -0500

    saving

--- schema.js
+++ schema.js
@@ -11,12 +11,26 @@ import {
 import {
   CoinType,
   MintType,
+  CompositionType,
+  DenominationType,
+  DesignerType,
+  EdgeType,
+  ImageType,
+  IssueType,
+  VarietyType,
 } from "./types";
 
 // Queries
 import {
   Mint,
   Coin,
+  Composition,
+  Denomination,
+  Designer,
+  Edge,
+  Image,
+  Issue,
+  Variety,
 } from "./queries.js";
 
 const QueryType = new GraphQLObjectType({
@@ -28,6 +42,60 @@ const QueryType = new GraphQLObjectType({
       args: {id: {type: new GraphQLNonNull(GraphQLID)}},
       resolve: (root, args) => Coin.findById(args.id).then( res => res.dataValues),
     },
+    composition: {
+      type: CompositionType,
+      args: {id: {type: new GraphQLNonNull(GraphQLID)}},
+      resolve: (root, args) => Composition.findById(args.id).then( res => res.dataValues),
+    },
+    compositions: {
+      type: new GraphQLList(CompositionType),
+      resolve: (root, args) => Composition.findAll()
+    },
+    denomination: {
+      type: DenominationType,
+      args: {id: {type: new GraphQLNonNull(GraphQLID)}},
+      resolve: (root, args) => Denomination.findById(args.id).then( res => res.dataValues),
+    },
+    denominations: {
+      type: new GraphQLList(DenominationType),
+      resolve: (root, args) => Denomination.findAll()
+    },
+    designer: {
+      type: DesignerType,
+      args: {id: {type: new GraphQLNonNull(GraphQLID)}},
+      resolve: (root, args) => Designer.findById(args.id).then( res => res.dataValues),
+    },
+    designers: {
+      type: new GraphQLList(DesignerType),
+      resolve: (root, args) => Designer.findAll()
+    },
+    edge: {
+      type: EdgeType,
+      args: {id: {type: new GraphQLNonNull(GraphQLID)}},
+      resolve: (root, args) => Edge.findById(args.id).then( res => res.dataValues),
+    },
+    edges: {
+      type: new GraphQLList(EdgeType),
+      resolve: (root, args) => Edge.findAll()
+    },
+    image: {
+      type: ImageType,
+      args: {id: {type: new GraphQLNonNull(GraphQLID)}},
+      resolve: (root, args) => Image.findById(args.id).then( res => res.dataValues),
+    },
+    images: {
+      type: new GraphQLList(ImageType),
+      resolve: (root, args) => Image.findAll()
+    },
+    issue: {
+      type: IssueType,
+      args: {id: {type: new GraphQLNonNull(GraphQLID)}},
+      resolve: (root, args) => Issue.findById(args.id).then( res => res.dataValues),
+    },
+    issues: {
+      type: new GraphQLList(IssueType),
+      resolve: (root, args) => Issue.findAll()
+    },
     mint: {
       type: MintType,
       args: {id: {type: new GraphQLNonNull(GraphQLID)}},
@@ -35,10 +103,17 @@ const QueryType = new GraphQLObjectType({
     },
     mints: {
       type: new GraphQLList(MintType),
-      resolve: (root, args) => Mint.findAll({
-        attributes: ['mark', 'name', 'id']
-      })
-    }
+      resolve: (root, args) => Mint.findAll()
+    },
+    variety: {
+      type: VarietyType,
+      args: {id: {type: new GraphQLNonNull(GraphQLID)}},
+      resolve: (root, args) => Variety.findById(args.id).then( res => res.dataValues),
+    },
+    varieties: {
+      type: new GraphQLList(VarietyType),
+      resolve: (root, args) => Variety.findAll()
+    },
   }),
 });
 
