82d25529eaf9b81b0252b28dc00cba459da87dec
theWickedWebDev <syoung@paintnite.com>
2017-08-29 19:37:37 -0400

    updated build and webpack

--- build.sh
+++ build.sh
@@ -6,6 +6,6 @@ chmod +x build.sh
 
 rm -rf /var/www/html/*
 
-rsync -auv /coin-collecting/react/public/ /var/www/html/
+rsync -auv /coin-collecting/public/ /var/www/html/
 
 curl -X POST --data "payload={\"text\": \":slack: Deployed Website\"}" https://hooks.slack.com/services/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
--- webpack.production.config.js
+++ webpack.production.config.js
@@ -35,14 +35,6 @@ module.exports = {
         NODE_ENV: '"production"'
       }
     }),
-    new webpack.optimize.UglifyJsPlugin({
-      compress: {
-        warnings: false,
-        screw_ie8: true,
-        drop_console: true,
-        drop_debugger: true
-      }
-    }),
     new webpack.optimize.OccurrenceOrderPlugin(),
     new ExtractTextPlugin({
       filename: 'style.css',
