0ae47bd2b5720c46d3b0d557bcdce7ccfad8a715
theWickedWebDev <syoung@paintnite.com>
2017-08-29 19:19:19 -0400

    updated build script

--- build.sh
+++ build.sh
@@ -1,11 +1,11 @@
 curl -X POST --data "payload={\"text\": \":slack: Starting Build - Web\"}" https://hooks.slack.com/services/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
 
-cd /home/coin-collecting
+cd /coin-collecting
 git pull origin master
 chmod +x build.sh
 
 rm -rf /var/www/html/*
 
-rsync -auv /home/coin-collecting/react/public/ /var/www/html/
+rsync -auv /coin-collecting/react/public/ /var/www/html/
 
 curl -X POST --data "payload={\"text\": \":slack: Deployed Website\"}" https://hooks.slack.com/services/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
