5e2fa28e7a76864681c2f054926ed920a04f9abf
theWickedWebDev <syoung@paintnite.com>
2017-08-28 21:47:01 -0400

    updated db

--- db/01-database.sql
+++ db/01-database.sql
@@ -18,6 +18,9 @@
 --
 -- Table structure for table `coins`
 --
+CREATE DATABASE coins_db;
+
+USE coins_db;
 
 DROP TABLE IF EXISTS `coins`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
