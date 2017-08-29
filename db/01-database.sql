9bd60b6a31ccaf0115425d2f7baf7674b2847a5d
theWickedWebDev <syoung@paintnite.com>
2017-08-29 00:24:54 -0400

    saving

--- db/01-database.sql
+++ db/01-database.sql
@@ -19,6 +19,7 @@
 -- Table structure for table `coins`
 --
 
+USE coins_db;
 DROP TABLE IF EXISTS `coins`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
