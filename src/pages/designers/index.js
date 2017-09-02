f8bf4b36059fbede57be36ad1c763a09759714a7
theWickedWebDev <syoung@paintnite.com>
2017-09-02 00:23:01 -0400

    added compositions page

--- src/pages/designers/index.js
+++ src/pages/designers/index.js
@@ -1,15 +1,42 @@
 import React, { PropTypes } from "react";
+import { compose, graphql, gql } from 'react-apollo';
+import {connect} from 'react-redux';
+import Spinner from '../../components/spinner';
+import './style.scss';
 import DefaultLayout from '../../layouts/default';
 
 class Designers extends React.Component {
-
   render() {
-    let { location } = this.props;
+    const { data, browser, location, me } = this.props;
+    const { designers } = data;
+    let classes = ['designers-page', browser.mediaType];
 
     return (
       <DefaultLayout location={location}>
-        <section className="designers-section">
-          <h1>Designers</h1>
+        <section className={classes.join(' ')}>
+          <article className={"main-article-no-admin"}>
+            <p className="results-header clearfix">
+              <span>Results ({designers ? designers.length : 0} of {designers ? designers.length : 0})</span>
+            </p>
+            { data.loading ? <Spinner/> : null }
+            <ul className="designers-list">
+              { designers && designers.length && !data.loading > 0 ?
+                designers.map(designer => {
+                  return (
+                    <li key={'designer:' + designer.id} className="designer-list-item">
+                      <p>
+                        <span className="name">
+                          { designer.name }
+                        </span>
+                      </p>
+                    </li>
+                  )
+                })
+                :
+                <p className="empty">Time to start making designers!</p>
+              }
+            </ul>
+          </article>
         </section>
       </DefaultLayout>
     );
@@ -17,7 +44,26 @@ class Designers extends React.Component {
 }
 
 Designers.propTypes = {
+  data: PropTypes.object,
   location: PropTypes.object,
+  me: PropTypes.object,
 };
 
-export default Designers;
+function mapStateToProps(state){
+  return {
+    browser: state.browser,
+    me: state.reducers.me,
+  }
+}
+
+export default compose(
+  connect(mapStateToProps),
+  graphql(gql`
+      query {
+          designers {
+              id
+              name
+          }
+      }
+  `),
+)(Designers);
