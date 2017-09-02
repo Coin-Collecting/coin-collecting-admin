0925ebf2ddd8d876af295069094374d0e7de67a6
theWickedWebDev <syoung@paintnite.com>
2017-09-02 00:20:47 -0400

    added denominations page

--- src/pages/denominations/index.js
+++ src/pages/denominations/index.js
@@ -1,15 +1,42 @@
 import React, { PropTypes } from "react";
+import { compose, graphql, gql } from 'react-apollo';
+import {connect} from 'react-redux';
+import Spinner from '../../components/spinner';
+import './style.scss';
 import DefaultLayout from '../../layouts/default';
 
 class Denominations extends React.Component {
-
   render() {
-    let { location } = this.props;
+    const { data, browser, location, me } = this.props;
+    const { denominations } = data;
+    let classes = ['denominations-page', browser.mediaType];
 
     return (
       <DefaultLayout location={location}>
-        <section className="denominations-section">
-          <h1>Denominations</h1>
+        <section className={classes.join(' ')}>
+          <article className={"main-article-no-admin"}>
+            <p className="results-header clearfix">
+              <span>Results ({denominations ? denominations.length : 0} of {denominations ? denominations.length : 0})</span>
+            </p>
+            { data.loading ? <Spinner/> : null }
+            <ul className="denominations-list">
+              { denominations && denominations.length && !data.loading > 0 ?
+                denominations.map(denomination => {
+                  return (
+                    <li key={'denomination:' + denomination.id} className="denomination-list-item">
+                      <p>
+                        <span className="name">
+                          { denomination.kind.toLowerCase().replace(/_/g, ' ') }
+                        </span>
+                      </p>
+                    </li>
+                  )
+                })
+                :
+                <p className="empty">Time to start making denominations!</p>
+              }
+            </ul>
+          </article>
         </section>
       </DefaultLayout>
     );
@@ -17,7 +44,27 @@ class Denominations extends React.Component {
 }
 
 Denominations.propTypes = {
+  data: PropTypes.object,
   location: PropTypes.object,
+  me: PropTypes.object,
 };
 
-export default Denominations;
+function mapStateToProps(state){
+  return {
+    browser: state.browser,
+    me: state.reducers.me,
+  }
+}
+
+export default compose(
+  connect(mapStateToProps),
+  graphql(gql`
+    query {
+      denominations {
+        id
+        kind
+        val
+      }
+    }
+  `),
+)(Denominations);
